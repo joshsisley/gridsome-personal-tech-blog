5d03f49cfb1e4ffa946947e4ee956b3d9bb2a5de
Josh Sisley <josh@joshuasisley.com>
2020-10-30 10:04:31 -0400

    fix to main file

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import 'prismjs/themes/prism-okaidia.css'
 
 import '~/resources/scss/main.scss'
+
+import 'prismjs/themes/prism-okaidia.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
